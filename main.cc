b4fce355e95182fc2ea368ab3ca822740244aa07
tyler <tmccown21@gmail.com>
2023-03-14 18:04:20 -0700

    serial monitoring check

--- main.cc
+++ main.cc
@@ -1,6 +1,9 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
 
-int main() {
+const uint8_t VL6180X_I2C_ADDR = 0x29;
+
+void blink() {
   const uint LED_PIN = PICO_DEFAULT_LED_PIN;
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -11,3 +14,13 @@ int main() {
     sleep_ms(250);
   }
 }
+
+void print_things() {
+  stdio_init_all();
+  while (true) {
+    printf("what the fuck printf\n");
+    sleep_ms(1000);
+  }
+}
+
+int main() { print_things(); }
