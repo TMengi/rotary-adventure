d67035b7d4c78490f534e6fcd107db13f5b8081c
tyler <tmccown21@gmail.com>
2023-03-19 11:57:32 -0700

    More double quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,10 @@ cwd="$(pwd)"
 root="$(git rev-parse --show-toplevel)"
 devel="/home/$(whoami)/devel/"
 
-if [[ ! -e $devel ]]; then
-  mkdir $devel
+if [[ ! -e "$devel" ]]; then
+  mkdir "$devel"
 fi
-cd $devel
+cd "$devel"
 
 # Install toolchain
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
@@ -34,7 +34,7 @@ if [[ ! -e pico-sdk ]]; then
   git clone https://github.com/raspberrypi/pico-sdk.git --branch master
   cd pico-sdk
   git submodule update --init
-  export PICO_SDK_PATH=$devel/pico/pico-sdk
+  export PICO_SDK_PATH="$devel/pico/pico-sdk"
 else
   print_header "Updating Pico SDK"
   cd pico-sdk
@@ -43,10 +43,10 @@ else
 fi
 
 # Run initial build
-cd $root
+cd "$root"
 print_header "Building project"
 ./build.sh
 
 echo -e "\n\e[32mBootstrap done\e[0m"
 
-cd $cwd
+cd "$cwd"
